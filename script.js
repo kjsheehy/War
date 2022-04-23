0ba95b6f0635a0f1fbe8b0cb80d0312bc0e952c5
kjsheehy <kjsheehy@gmail.com>
2022-04-23 19:23:02 -0400

    Card deck creation implemented

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const player1Deck;
+const player2Deck;
+
+const deal = function () {
+  // Create standard deck as array of card objects
+  let cards = [];
+  for (let i = 2; i < 15; i++) {
+    cards.push({ value: i, suit: "Hearts" });
+    cards.push({ value: i, suit: "Spades" });
+    cards.push({ value: i, suit: "Diamonds" });
+    cards.push({ value: i, suit: "Clubs" });
+  }
+
+  // Shuffle deck array
+
+  // Split deck array between the 2 players
+
+};
+
+
