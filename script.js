e86239b2b0a83fda0897de29d18c19aa815f5713
kjsheehy <kjsheehy@gmail.com>
2022-04-23 20:52:36 -0400

    implemented optimized O(n) version of Fisher-yates to shuffle deck array in deal function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const player1Deck;
-const player2Deck;
+//const player1Deck;
+//const player2Deck;
 
 const deal = function () {
   // Create standard deck as array of card objects
@@ -12,11 +12,14 @@ const deal = function () {
     cards.push({ value: i, suit: "Diamonds" });
     cards.push({ value: i, suit: "Clubs" });
   }
-
-  // Shuffle deck array
+  console.log(cards);
+  // Shuffle deck array using an optimized (to O(n)) version of Fisher-Yates
+  for (let x = cards.length - 1; x >= 0; x--) {
+    let index = Math.floor(Math.random() * x);
+    let temp = cards[x];
+    cards[x] = cards[index];
+    cards[index] = temp;
+  }
 
   // Split deck array between the 2 players
-
 };
-
-
